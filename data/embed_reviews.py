c9f7f01d65581ca953db0505719f940b1b20eae7
Aly <alyshariff2810@gmail.com>
2024-09-14 18:40:17 -0400

    Done with embedding script

--- data/embed_reviews.py
+++ data/embed_reviews.py
@@ -15,15 +15,19 @@ SBERT_embedding_model = SentenceTransformer('sentence-transformers/all-MiniLM-L6
 if __name__ == "__main__":
     file = "reviews.json"
     with open(file) as f:
-        text_data = json.loads(f)
+        text_data = json.load(f)
         f.close()
+    i = 0
     for entry in text_data:
+        i += 1
         try:
             with open('reviews_embedded.json', 'r') as json_file:
                 reviews = json.load(json_file)
         except FileNotFoundError:
             reviews = []
         review_encoded = SBERT_embedding_model.encode(entry)
-        reviews.append({"embedding": entry, "sentence": entry})
+        reviews.append({"embedding": review_encoded.tolist(), "sentence": entry})
         with open('reviews_embedded.json', 'w') as json_file:
-            json.dump(reviews, json_file, indent=4)
+            json.dump(reviews, json_file)
+        print(i)
+        
