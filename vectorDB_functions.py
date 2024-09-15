5017fc9b4a9563d0af27b055b5c913f8daca268b
Aly <alyshariff2810@gmail.com>
2024-09-15 02:01:15 -0400

    created the VectorDB functions

--- vectorDB_functions.py
+++ vectorDB_functions.py
@@ -10,16 +10,28 @@ pc = Pinecone(api_key=os.environ.get("PINECONE_API_KEY"))
 index = pc.Index("ranked")
 
 
-def update_profile(autoencoder, MLP, profile: list, review: str):
+def update_profile(autoencoder, MLP, profile_id, profile: list, review: str, product_id):
     '''
     profile: customer profile (not the latents)
+    Updates the profile based on the new review
     '''
     review = SBERT_embedding_model.encode(review)
     latent_profile = autoencoder.encode(profile)
     embeddings = MLP(review)
     new_latents = latent_profile + embeddings
     new_profile = autoencoder.decode(latent_profile)
-    return new_profile, new_latents
+    response = index.fetch(ids=[profile_id])
+    current_metadata = response['vectors'][profile_id]['metadata']
+    product_ids = current_metadata["product_ids"]
+    product_ids.append(product_id)
+    index.update(
+    id=new_latents,
+    set_metadata={
+        "product_ids": product_ids,  # Update the 'tags' field in metadata
+        "profile_id": profile_id
+        }
+    )
+    return new_profile
 
 def upload_profile(profile, product_ids, profile_id, autoencoder_model):
     value = autoencoder_model.encode(profile)
@@ -33,12 +45,7 @@ def upload_profile(profile, product_ids, profile_id, autoencoder_model):
     ],)
 
 def fetch_profile(profile_id):
-    return index.fetch([profile_id])
-
-def update_profile(profile_id, profile, autoencoder_model):
-    value = autoencoder_model.encode(profile)
-    index.update(id=profile_id, values=value)
-
+    return index.fetch([profile_id])['vectors'][profile_id]
 
 def query_profiles(product_id, profile_id):
     profile_vector = fetch_profile(profile_id)
